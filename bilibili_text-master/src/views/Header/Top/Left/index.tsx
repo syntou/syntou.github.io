f7064ca04501e3e9f0fbb0047b82efb7626cde35
syntou <kumaizi23@163.com>
2020-01-21 03:46:15 +0800

    bi

---
+++ bilibili_text-master/src/views/Header/Top/Left/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import * as style from "./style.scss"
+import Game from "./Game"
+import Cartoon from "./Cartoon"
+import App from "./App"
+export default function Left(){
+    return <div className={style.a}>
+        <a href="#" className={style.b}>主站</a>
+        <a href="#" className={style.b}>番剧</a>
+        <Game />
+        <a href="#" className={style.b}>直播</a>
+        <a href="#" className={style.b}>会员购</a>
+        <Cartoon />
+        <a href="#" className={style.b}>赛事</a>
+        <a href="#" className={style.b}>BML</a>
+        <a href="#" className={style.b}>下载app</a>
+        <App />
+    </div>
+}
